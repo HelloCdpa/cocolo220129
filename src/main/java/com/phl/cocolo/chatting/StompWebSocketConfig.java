da6871a0b852307374ce16315e61b14a58fe9d8d
HelloCdpa <92144783+HelloCdpa@users.noreply.github.com>
2022-03-08 13:13:01 +0900

    채팅 STOMP 연동

---
+++ src/main/java/com/phl/cocolo/chatting/StompWebSocketConfig.java
@@ -0,0 +1,37 @@
+package com.phl.cocolo.chatting;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+//Stomp를 사용하기위해 선언하는 어노테이션
+@EnableWebSocketMessageBroker
+@Configuration
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    //endpoint를 /stomp로 하고, allowedOrigins를 "*"로 하면 페이지에서
+    //Get /info 404 Error가 발생한다. 그래서 아래와 같이 2개의 계층으로 분리하고
+    //origins를 개발 도메인으로 변경하니 잘 동작하였다.
+    //이유는 왜 그런지 아직 찾지 못함
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/stomp/chat")
+                .setAllowedOrigins("http://localhost:8095")
+                .withSockJS();
+    }
+
+    /*어플리케이션 내부에서 사용할 path를 지정할 수 있음*/
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+    // Client 에서 SEND 요청을 처리
+    //Spring docs 에서는 /topic, /queue로 나오나 편의상 /pub, /sub로 변경
+        registry.setApplicationDestinationPrefixes("/pub");
+        //해당 경로로 SimpleBroker를 등록.
+        // SimpleBroker는 해당하는 경로를 SUBSCRIBE하는 Client에게 메세지를 전달하는 간단한 작업을 수행
+        registry.enableSimpleBroker("/sub");
+        //enableStompBrokerRelay
+        //SimpleBroker의 기능과 외부 Message Broker( RabbitMQ, ActiveMQ 등 )에 메세지를 전달하는 기능을 가짐
+    }
+}
---
+++ src/main/java/com/phl/cocolo/controller/StompChatController.java
@@ -0,0 +1,35 @@
+package com.phl.cocolo.controller;
+
+import com.phl.cocolo.dto.ChatMessageDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class StompChatController {
+
+    private final SimpMessagingTemplate template; //특정 Broker로 메세지를 전달
+
+    //Client 가 SEND 할 수 있는 경로
+    //stompConfig 에서 설정한 applicationDestinationPrefixes 와 @MessageMapping 경로가 병합됨
+    //"/pub/chat/enter"
+    @MessageMapping(value = "/chat/enter")
+    public void enter(ChatMessageDTO message) {
+        message.setMessage(message.getWriter() + "님이 채팅방에 참여하였습니다.");
+        template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+    }
+
+    @MessageMapping(value = "/chat/message")
+    public void message(ChatMessageDTO message) {
+        template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+    }
+//    @MessageMapping 을 통해 WebSocket 으로 들어오는 메세지 발행을 처리한다.
+//    Client 에서는 prefix 를 붙여 "/pub/chat/enter"로 발행 요청을 하면
+//    Controller 가 해당 메세지를 받아 처리하는데,
+//    메세지가 발행되면 "/sub/chat/room/[roomId]"로 메세지가 전송되는 것을 볼 수 있다.
+//    Client 에서는 해당 주소를 SUBSCRIBE 하고 있다가 메세지가 전달되면 화면에 출력한다.
+//    이때 /sub/chat/room/[roomId]는 채팅방을 구분하는 값이다.
+//    기존의 핸들러 ChatHandler 의 역할을 대신 해주므로 핸들러는 없어도 된다.
+}
