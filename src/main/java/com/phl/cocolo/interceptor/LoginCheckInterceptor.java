795f482363d96fab933c3a0355c9a0048654bf77
HelloCdpa <92144783+HelloCdpa@users.noreply.github.com>
2022-03-15 16:19:30 +0900

    인터셉터 적용

--- src/main/java/com/phl/cocolo/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/phl/cocolo/interceptor/LoginCheckInterceptor.java
@@ -1,4 +1,35 @@
 package com.phl.cocolo.interceptor;
 
-public class LoginCheckInterceptor {
+import com.phl.cocolo.common.SessionConst;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class LoginCheckInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+        //사용자가 요청한 주소
+        String requestURI = request.getRequestURI();
+        //session은 request 객체 안에 들어있음 -> 서블릿이 관리해 줌
+        // 서블릿이 있어야 웹서버 구축 - 스프링부트, 스프링 프레임 워크로 서블릿을 편하게 쓴다!
+        //세션을 가져옴
+        HttpSession session = request.getSession();
+        //세션에 로그인 정보가 있는지 확인(세션 로그인 값이 있으면~ 로그인 한 것 없으면 ~안 한 것)
+        if(session.getAttribute(SessionConst.LOGIN_EMAIL) == null){
+            //미로그인 상태
+            //로그인 하지 않은 경우 바로 로그인 페이지로 보내고 로그인 후 요청 화면을 보여줌.
+            //requestURI 사용자가 요청한 주소값
+            session.setAttribute("redirectURL", requestURI);
+            response.sendRedirect("/member/login");
+
+            return false;
+        } else {
+            //로그인 상태
+            return true;
+        }
+    }
+
 }
--- src/main/java/com/phl/cocolo/interceptor/WebConfig.java
+++ src/main/java/com/phl/cocolo/interceptor/WebConfig.java
@@ -1,5 +1,22 @@
 package com.phl.cocolo.interceptor;
 
 
-public abstract class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    //로그인 여부에 따른 접속 가능 페이지 구분
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        //체인 메서드
+        registry.addInterceptor(new LoginCheckInterceptor()) //만든 LoginCheckInterceptor 클래스 내용을 넘김
+                .order(1) // 해당 인터셉터가 적용되는 순서 (몇번째로 적용시켜줄까? 스프링이 우선순위를 배정해줌)
+                .addPathPatterns("/**") //해당 프로젝트의 모든 주소에 대해 인터셉터를 적용
+                .excludePathPatterns("/","/member/save","/member/login","/member/logout","/assets/**","/member/emailDuplication",
+                        "/board_upload/**","/member_upload/**","/class_upload/**","/board/**","/onClass/**","/study/","/mentoring/",
+                        "/member/NickNameDuplication","/board/search/**","/board/cateBoard/**"
+                        ); //그 중에 이 주소는 제외
+    }
+
 }
